f2bfe3025400e2cac4f4b0708fb2224698e64539
Robert-Chen12 <40195326+Robert-Chen12@users.noreply.github.com>
2018-06-26 16:09:17 +0900

    Update apple.c

--- src/apple.c
+++ src/apple.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+     int i;
+     int y = 0;
+     for (i =0; i<n; i++) {
+            y += ((A[i]-1) /m) +1;
+     }
+     return y <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 100000000;
+  while ( ub-lb > 1) {
+    int m = (lb+ub) / 2;
+    if (p(m)) {
+        ub = m;
+    }
+    else {
+        lb = m;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
