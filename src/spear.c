3aeca6fce3cece22ce0a822ff058fa76ba283318
Robert-Chen12 <40195326+Robert-Chen12@users.noreply.github.com>
2018-06-26 16:12:30 +0900

    Update spear.c

--- src/spear.c
+++ src/spear.c
@@ -4,6 +4,14 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+     int i;
+     int y = 0;
+     for (i =0; i<n; i++) {
+            y += A[i] /m;
+     }
+     return y < k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +19,17 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = 0;
+  ub = 100000001;
+  while ( ub-lb > 1) {
+    int m = (lb+ub) / 2;
+    if (p(m)) {
+        ub = m;
+    }
+    else {
+        lb = m;
+    }
+  }
+  printf("%d\n",lb);
   return 0;
 }
