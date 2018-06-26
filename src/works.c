df8a7b8a897f97aa2df053edbdfca50b6e103b20
Robert-Chen12 <40195326+Robert-Chen12@users.noreply.github.com>
2018-06-26 16:13:25 +0900

    Update works.c

--- src/works.c
+++ src/works.c
@@ -4,6 +4,21 @@ int n;
 int k;
 int A[100000];
 
+int p(int m){
+     int i;
+     int y = 0;
+     int peo = 1; // the number of people
+     for (i =0; i<n; i++) {
+        if ( y + A[i]<= m ){
+            y += A[i];
+        }
+        else {
+            peo ++;
+            y = A[i];
+        }
+     }
+     return peo <= k;
+}
 
 int main(){
   int i, lb, ub;
@@ -11,7 +26,25 @@ int main(){
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
-
-
+  lb = -1;
+  ub = 1000001;
+  if (n = k){
+    for(i = 0; i < n; i++){
+        if (A[i]>A[i+1]){
+            A[i+1]=A[i];
+        }
+    }
+    ub = A[n-1];
+  }
+  while ( ub-lb > 1) {
+    int m = (lb+ub) / 2;
+    if (p(m)) {
+        ub = m;
+    }
+    else {
+        lb = m;
+    }
+  }
+  printf("%d\n",ub);
   return 0;
 }
